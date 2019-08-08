f2937db0e79f8c5eac90a7a6ac201f286ced8a61
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-08-08 11:19:13 -0400

    grabbed articles and attached data

--- Article/newArticle.js
+++ Article/newArticle.js
@@ -112,3 +112,16 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 */
+
+const accordion = document.querySelector("articles");
+data.forEach(data => {
+  accordion.appendChild(
+    createPanel(
+      data.title,
+      data.date,
+      data.firstParagraph,
+      data.secondParagraph,
+      data.thridParagraph
+    )
+  );
+});
